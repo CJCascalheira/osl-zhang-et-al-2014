dbaa175f3d0a86ef3914c4dbf802af54f4f921c6
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2018-12-03 14:07:39 -0500

    Finish manipulation check, add auxiliary analyses

--- src/zhang.R
+++ src/zhang.R
@@ -39,6 +39,10 @@ zhang_clean <- within(zhang_clean, {
 # Number of participants in each condition
 zhang_clean %>% count(condition)
 
+# Split data by condition for descriptive statistics
+ordinary <- zhang_clean %>% filter(condition == "ordinary")
+extraordinary <- zhang_clean %>% filter(condition == "extraordinary")
+
 ####### EXTRAORDINARINESS MANIPULATION CHECK #######
 
 # Create analysis of variance object
@@ -47,6 +51,70 @@ t1_extra_aov <- aov(t1_extra ~ condition, data = zhang_clean)
 # Summarize one-way ANOVA
 Anova(t1_extra_aov)
 
+# Descriptive statistics
+zhang_clean %>%
+  group_by(condition) %>%
+  summarize(
+    mean = mean(t1_extra),
+    sd = sd(t1_extra),
+    n = n(),
+    upper = mean - (1.96 * (sd / sqrt(n))),
+    lower = mean + (1.96 * (sd / sqrt(n)))
+  )
 
+# Tidy aov object
+(t1_extra_tidied <- tidy(t1_extra_aov))
+
+# Partial eta-squared
+(t1_extra_tidied[1, 3] / (t1_extra_tidied[1, 3] + t1_extra_tidied[2, 3])) %>%
+  pull()
+
+#### Outliers?
+ggplot(zhang_clean, aes(x = condition, y = t1_extra)) +
+  geom_boxplot()
+
+#### Normality?
+shapiro.test(residuals(t1_extra_aov))
+
+#### Homoscedasticity?
+leveneTest(t1_extra ~ condition, data = zhang_clean)
 
 ####################################################################################
+
+####### EXTRAORDINARINESS OVER TIME BY CONDITION #######
+
+#### Outliers?
+
+#### Normality?
+
+#### Homoscedasticity?
+
+#### Sphericity?
+
+####################################################################################
+
+####### CURIOSITY OVER TIME BY CONDITION #######
+
+#### Outliers?
+
+#### Normality?
+
+#### Homoscedasticity?
+
+#### Sphericity?
+
+####################################################################################
+
+####### INTEREST OVER TIME BY CONDITION #######
+
+#### Outliers?
+
+#### Normality?
+
+#### Homoscedasticity?
+
+#### Sphericity?
+
+####################################################################################
+
+####### VISUALIZE #######
